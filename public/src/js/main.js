ae1aeaac7e21a9d50c01d963c9d7c56277a5f145
xiaohan2013 <terryfi@163.com>
2015-11-27 10:49:52 +0800

    关于配置第三方库

---
+++ public/src/js/main.js
@@ -0,0 +1,17 @@
+require.config({
+    baseUrl:"js/",
+    paths:{
+        backbone:"../../vendor/backbone/backbone-min",
+        bootstrap:"../../vendor/bootstrap/dist/js/bootstrap.min",
+        jquery:"../../vendor/jquery/dist/jquery.min",
+        css:"../../vendor/require-css/css-min",
+        tpl:"../../vendor/requirejs-underscore-tpl/underscore-tpl",
+        underscore:"../../vendor/underscore/underscore-min"
+    },
+    shim:{
+
+    },
+    waitSeconds:0
+});
+
+require([]);
